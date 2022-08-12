c3fc74db796d501143cd04471d3f33c2eb60e91c
Aaron Key <AaronMikelKey@gmail.com>
2022-08-12 12:06:25 -0400

    Added index route

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express =require('express')
+const path = require('path')
 
 const app = express()
 
 app.get('/', (req, res) => {
-	res.send('Hello world!')
+	res.sendFile(path.join(__dirname, './Develop/public/index.html'))
 })
 
 module.exports = app
